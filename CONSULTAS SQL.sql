0aef542c734b663306e0ae14c68aceb6a9c6236e
Diego Frank Lipa Choque <d.diegolipa@gmail.com>
2025-03-26 14:16:41 -0500

    TAREA 01

---
+++ CONSULTAS SQL.sql
@@ -0,0 +1,12 @@
+--::::ACTIVIDAD 001::::
+--NOMBRE: Diego Lipa
+--nombre
+--apellido paterno y materno juntos ()
+--edad
+--tipo documento (DNI)
+--celular
+SELECT p.NOMBRES, p.APELLIDO_PATERNO , p.APELLIDO_MATERNO,
+p.FECHA_NACIMIENTO ,p.ID_TIPO_DOCUMENTO , p.CELULAR 
+FROM PERSONAS p ;
+
+SELECT * FROM TIPO_DOCUMENTO td ;
